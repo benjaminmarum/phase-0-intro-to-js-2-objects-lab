3e6cb3009c5d6ae77a3b5fdd3a46119dfe1c39a7
Ben Marum <ben.j.marum@gmail.com>
2023-04-25 21:45:11 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+// Write your solution in this file
+
+const employee = {
+    name: `Ed`,
+    streetAddress: `333 West St`
+};
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    const employeeObj = { ...employee };
+    employeeObj[key] = value;
+    return employeeObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee,key){
+    const employeeObj = { ...employee };
+    delete employeeObj[key];
+    return employeeObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+    return employee;
+}
